15315aa7c2612ec65afd5c82669ecadd5acd5b33
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-20 12:02:28 +0200

    created the controllers and the views

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,10 @@
 class Ingredient < ActiveRecord::Base
   has_many :doses, dependent: :destroy
   has_many :cocktails, through: :doses
+
+  ingredients = %w(lemon ice 'mint leaves' redbull jagermeister sugar tonic gin rhum)
+  INGREDIENTS = ingredients.map { |ingredient| ingredient.capitalize }
+
   validates :name, uniqueness: true, presence: true
+  validates :name, inclusion: { in: INGREDIENTS, message: "Not one of the listed ingredients" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   resources :cocktails do
     resources :doses, only: [:new, :create, :destroy]
   end
