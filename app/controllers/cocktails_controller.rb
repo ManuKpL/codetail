c957f38a30344e885567d1a6085fcf1f6510e3a7
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-20 12:22:39 +0200

    created the actions of the cocktails controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,22 +1,41 @@
 class CocktailsController < ApplicationController
+before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail= Cocktail.new(cocktail_params)
+    @cocktail.save ? (redirect_to cocktail_path(@cocktail)) : (render :new)
   end
 
   def edit
   end
 
   def update
+    @cocktail.update(cocktail_params) ? (redirect_to cocktail_path(@cocktail)) : (render :edit)
   end
 
   def destroy
+    @cocktail.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create, :destroy]
   end
 
+  root 'ingredients#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
