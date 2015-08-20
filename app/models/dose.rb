969b8e9020b5d05378a5d56bc9672b6649ca78c8
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-20 11:14:43 +0200

    passed all validations of three models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ActiveRecord::Base
   belongs_to :cocktail
   belongs_to :ingredient
+  validates_presence_of :description, :cocktail_id, :ingredient_id
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id, message: "Only one dose allowed for the same cocktail + ingredient couple" }
 end
