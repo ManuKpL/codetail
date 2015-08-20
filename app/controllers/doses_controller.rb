60788cfb5b8ac2427dd2e1fece1eb5e51bd9153f
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-20 15:56:34 +0200

    added some style and views

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,36 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, :set_ingredient
+  before_action :set_dose, only: [:destroy]
+
   def new
+    @dose = Dose.new
   end
 
   def create
+    @dose = @cocktail.doses.build(dose_params)
+    @dose.save ? (redirect_to cocktail_path(@cocktail)) : (render :new)
   end
 
   def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:ingredient_id])
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,19 @@
 ingredients = %w(lemon ice 'mint leaves' redbull jagermeister sugar tonic gin rhum)
 ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
 
-Cocktail.create(name: "mojito")
-Cocktail.create(name: "jager bomb")
-Cocktail.create(name: "gin tonic")
+cocktails = [
+  {
+    name: "mojito",
+    picture: "http://www.whouhou.com/wp-content/uploads/2015/08/tray-of-mojitos.jpg"
+  },
+  {
+    name: "jager bomb",
+    picture: "http://theseasonaire.com/wp-content/uploads//Jagerbomb-470-wplok.jpg"
+  },
+  {
+    name: "gin tonic",
+    picture: "http://www.slate.com/content/dam/slate/articles/life/drink/2013/08/130826_DRINK_gin-and-tonic.jpg.CROP.article568-large.jpg"
+  }
+]
+
+cocktails.each { |cocktail| Cocktail.create(cocktail) }
